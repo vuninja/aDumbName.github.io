9d3573eb4bf30be6e4a40498f97e914c3c4c4b2a
Weiyan Zhu <weiyanz@uci.edu>
2019-02-13 15:24:34 -0800

    display function

--- Scripts/database.js
+++ Scripts/database.js
@@ -1,6 +1,21 @@
+var datas = ["test1","test2","test3"];
+
 function view() {
     var x = document.getElementById("first_name").value;
     document.getElementById("displayName").innerHTML = x;
     var y = document.getElementById("email").value;
     document.getElementById("displayEmail").innerHTML = y;
   }
+
+function display()
+{
+    for(var x = 0; x < datas.length; ++x)
+    {
+        var field = document.createElement("P"); 
+        var text = document.createTextNode(datas[x]);
+        field.appendChild(text);
+        //var br = document.createElement("BR"); 
+        document.getElementById("display_window").appendChild(field);
+        //document.getElementById("display_window").appendChild(br);
+    }
+}
